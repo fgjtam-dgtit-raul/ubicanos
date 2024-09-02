0f7e0dc51865f0796b917b0a11efbc3609aa9275
J. Salvador Rangel <juan.rangel@fgjtam.gob.mx>
2024-09-02 13:30:57 -0600

    📦 NEW: Add layout for movil view

--- app/Http/Controllers/MapController.php
+++ app/Http/Controllers/MapController.php
@@ -5,22 +5,64 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
 use Inertia\Inertia;
+use Jenssegers\Agent\Agent;
 
 class MapController extends Controller
 {
     public function index(){
 
+        // * set center of the map
         $centerMap = array(24.853449, -98.827877);
         
+
         // * get municipalities data
         $municipalities = \App\Models\Municipality::all();
 
-        // * load tam geometries
+        // * load municipalities polygons
+        $municipalitiesGeom = $this->loadMunicipalitiesPolygons();
+
+        // * load officess locations
+        $officesLocations = $this->loadOfficesLocations();
+        
+        // * return view based on movil or desktop
+        $agent = new Agent();
+        if ($agent->isMobile()) {
+            
+            // * return view
+            return Inertia::render("Map/IndexMovil", [
+                "title" => "Hola mundo",
+                "centerMap" => $centerMap,
+                "municipalities" => $municipalities,
+                "municipalitiesGeom" => $municipalitiesGeom
+            ]);
+
+        } else {
+            
+            // * return view
+            return Inertia::render("Map/Index", [
+                "title" => "Hola mundo",
+                "centerMap" => $centerMap,
+                "municipalities" => $municipalities,
+                "municipalitiesGeom" => $municipalitiesGeom,
+                "officesLocations" => $officesLocations
+            ]);
+        }
+
+    }
+
+    
+    /**
+     * loadMunicipalitiesPolygons
+     *
+     * @return array
+     */
+    private function loadMunicipalitiesPolygons(){
+        // * load municipalities geometries
         $contents = File::get('tam-geometries.json');
         $jsonData = json_decode(json: $contents, associative: true)["features"];
 
         // * process the json data
-        $municipalitiesGeom = array();
+        $municipalitiesPolygons = array();
         foreach ($jsonData as $element) {
             $municipalitiesGeomItem = array();
             $municipalitiesGeomItem["properties"] = $element["properties"];
@@ -29,17 +71,22 @@ class MapController extends Controller
                 array_push( $geometryItems, array( $geomItem[1], $geomItem[0] ));
             }
             $municipalitiesGeomItem["geometry"] = $geometryItems;
-            array_push( $municipalitiesGeom, $municipalitiesGeomItem);
+            array_push( $municipalitiesPolygons, $municipalitiesGeomItem);
         }
 
-        // * return view
-        return Inertia::render("Map/Index", [
-            "title" => "Hola mundo",
-            "centerMap" => $centerMap,
-            "municipalities" => $municipalities,
-            "municipalitiesGeom" => $municipalitiesGeom
-        ]);
-
-
+        return $municipalitiesPolygons;
     }
+    
+    /**
+     * loadOfficesLocations
+     *
+     * @return array
+     */
+    private function loadOfficesLocations(){
+        // * load file
+        $contents = File::get('offices.json');
+        $officessData = json_decode(json: $contents, associative: true)["offices"];
+        return $officessData;
+    }
+
 }
--- resources/js/Pages/Map/IndexMovil.vue
+++ resources/js/Pages/Map/IndexMovil.vue
@@ -12,8 +12,7 @@ const props = defineProps({
         default: [23.739622, -99.147968]
     },
     municipalities: Array,
-    municipalitiesGeom: Object,
-    officesLocations: Array,
+    municipalitiesGeom: Object
 });
 
 const map = ref({});
@@ -25,7 +24,7 @@ const bounds = ref({});
 const municipalitySelected = ref(null);
 
 onMounted(()=>{
-    
+
     // var map = L.map('map').setView([51.505, -0.09], 13);
 
     initializeMap();
@@ -33,7 +32,7 @@ onMounted(()=>{
 })
 
 function initializeMap() {
-    
+
     // * laod map
     document.map = L.map('map', {
         center: L.latLng( props.centerMap[0], props.centerMap[1]),
@@ -57,8 +56,6 @@ function initializeMap() {
     }).addTo(document.map);
 
     drawGeometries();
-
-    drawPushpins();
 
 }
 
@@ -105,14 +102,10 @@ function resetMapPosition(e){
     municipalitySelected.value = null;
 }
 
-function drawPushpins(){
-    console.dir( props.officesLocations);
-    props.officesLocations.forEach(element => {
-        L.marker( L.latLng(element[0], element[1]))
-            .addTo(document.map)
-            .bindPopup( element[3])
-            .openPopup();
-    });
+function drawPushpin(){
+    // L.marker( props.centerMap).addTo(map.value)
+    //     .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+    //     .openPopup();
 }
 
 function handleMunicipalityListItem(municipality){
@@ -152,45 +145,42 @@ function calculateCenter(coordinates){
 
     <AuthenticatedLayout>
 
-        <div class="max-w-screen-2xl mx-auto p-2 w-full h-full gap-2 grid grid-cols-map grid-rows-map" >
+        <div class="p-0 w-full h-full gap-0 flex flex-col items-center">
 
-            <div class="h-full outline outline-1 outline-gray-200 rounded-lg bg-white overflow-y-auto row-span-2 select-none">
-                <ul class="">
-                    <li v-for="m in municipalities" v-on:click="handleMunicipalityListItem(m)" class="border-b p-2 rounded-b-lg hover:bg-gray-100">
-                        
-                        <h2 class="uppercase py-2 text-lg font-semibold sticky top-0 bg-inherit cursor-pointer">{{m.name}}</h2>
+            <div class="w-full h-full outline outline-1 outline-gray-200 bg-emerald-100 overflow-clip">
+                <div id="map" class="w-full h-full" />
 
+                <div v-if="municipalitySelected" class="fixed top-[4rem] right-0 w-full h-[10rem] opacity-50 outline outline-1 outline-gray-200 rounded-lg bg-blue-100 flex flex-col z-[999]">
+                    <div class="flex">
+                        <svg v-on:click="resetMapPosition" fill="currentColor" aria-hidden="true" data-prefix="fas" data-icon="times" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512" class="cursor-pointer hover:text-gray-900 hover:bg-white ml-auto w-5 h-5 p-1 svg-inline--fa fa-times fa-w-11 fa-7x"><path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z" class=""></path></svg>
+                    </div>
+                    <div class="w-full h-full overflow-auto">
+                        <div v-for="key in Object.keys(municipalitySelected)" class="flex gap-2 border-b border-gray-400">
+                            <div>{{ key }}</div>
+                            <div>{{ municipalitySelected[key] }}</div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="w-full h-[16rem] outline outline-1 outline-gray-200 bg-white overflow-y-auto row-span-2 select-none">
+                <ul class="h-full">
+                    <li v-for="m in municipalities" v-on:click="handleMunicipalityListItem(m)" class="border-b p-1 rounded-b-lg hover:bg-gray-100">
+                        <h2 class="uppercase py-0.5 text-sm font-semibold sticky top-0 bg-inherit cursor-pointer">{{m.name}}</h2>
                         <ul class="mt-2 space-y-2">
                             <li v-for="l in m.locations">
                                 <p class="text-sm text-gray-600 hover:underline cursor-pointer flex items-center">
                                     <svg fill="currentColor" class="h-3 mx-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM432 256c0 79.5-64.5 144-144 144s-144-64.5-144-144s64.5-144 144-144s144 64.5 144 144zM288 192c0 35.3-28.7 64-64 64c-11.5 0-22.3-3-31.6-8.4c-.2 2.8-.4 5.5-.4 8.4c0 53 43 96 96 96s96-43 96-96s-43-96-96-96c-2.8 0-5.6 .1-8.4 .4c5.3 9.3 8.4 20.1 8.4 31.6z"/></svg>
-                                    
                                     <div>{{ l.title }}</div>
                                 </p>
                             </li>
                         </ul>
-
                     </li>
                 </ul>
             </div>
 
-            <div class="h-full outline outline-1 outline-gray-200 rounded-lg bg-emerald-100 overflow-clip" :class="[municipalitySelected ?'row-span-1' :'row-span-2']">
-                <div id="map" class="w-full h-full" />
-            </div>
-
-            <div v-if="municipalitySelected" class="outline outline-1 opacity-50 outline-gray-200 rounded-lg bg-blue-100 flex flex-col">
-                <div class="flex">
-                    <svg v-on:click="resetMapPosition" fill="currentColor" aria-hidden="true" data-prefix="fas" data-icon="times" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512" class="cursor-pointer hover:text-gray-900 hover:bg-white ml-auto w-5 h-5 p-1 svg-inline--fa fa-times fa-w-11 fa-7x"><path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z" class=""></path></svg>
-                </div>
-                <div class="w-full h-full overflow-auto">
-                    <div v-for="key in Object.keys(municipalitySelected)" class="flex gap-2 border-b border-gray-400">
-                        <div>{{ key }}</div>
-                        <div>{{ municipalitySelected[key] }}</div>
-                    </div>
-                </div>
-            </div>
-            
         </div>
-        
+
     </AuthenticatedLayout>
 </template>
