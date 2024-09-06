22be06e781b79a3d00e9672c10821ce9efbfbce1
Raul Gomez <raul.bautista@fgjtam.gob.mx>
2024-09-05 19:03:46 -0600

    Fix: Favicon url

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,9 +6,9 @@
 
         <title inertia>{{ config('app.name', 'Ubícanos') }}</title>
 
-        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/apple-touch-icon.png') }}">
-        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/favicon-32x32.png') }}">
-        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicon-16x16.png') }}">
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/apple-touch-icon.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/favicon-16x16.png') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
